2502f937aec96cf5167f597827aebec2a979089b
Dan <DanielMooreRealPerson@gmail.com>
2020-11-29 18:13:48 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,43 @@ class School
         @name = name
 
         @roster = {}
-    
-    
+    end
+
+    def add_student(student, grade)
+        #binding.pry
+
+        roster[grade] ||= []
+        roster[grade] << student
+
+        
+
+    end
+
+    def grade(grade)
+        roster[grade]
+
+        
+
+
+    end
+
+    def sort
+
+       roster.each do | grade, student | 
+        student.sort!
+       end
+        
+
+        
     end
 
 end
+
+#Be able to create new school names (initialize)
+#Create empty roster
+#place hashes into roster
+#hashes are grade => [names]
+#add names to array [names]
+#add grades with their own arrays
+#able to retrieve students from grade
+#able to sort students by grade
